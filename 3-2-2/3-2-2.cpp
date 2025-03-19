9c4b2bae981f22dfb3c91bed5240a226c2af5437
rehomik <re.ho.mik@gmail.com>
2025-03-19 19:51:04 +0900

    설명을 위한 코드 추가.

--- 3-2-2/3-2-2.cpp
+++ 3-2-2/3-2-2.cpp
@@ -1,38 +1,56 @@
 #include <iostream>
 
-void SampleFunc(int* src);
-int* SampleFunc();
+void SampleFunc1(int* src);
+int* SampleFunc2();
 
 int main()
 {
+	// 동적할당(c++ 문법), malloc() <- c버전.
+	// new int의 결과는 주소(할당된 메모리의 주소)
+	// 정적할당 | 동적할당 <- 메모리 할당 방식의 2가지 종류.
+	// int a | int* a = new int;
+	// float b | float* b = new float;
+	// int c[10] | int* c = new int[10];
+	// 함수(영역) 종료시 유지X(메모리 소멸, 자동 소멸) | 함수(영역) 종료시 유지O(메모리 소멸X, 수동 소멸)
+	// 
+	// 영역 == scope
+	// 영역 안에서 할당된 정적변수(메모리)는 유지되지 않는다.
+	// 문법({ })은 모두 영역을 열고 닫는 문법.
+
 	int* a = new int;
 	*a = 10;
-
 	std::cout << "a: " << a << std::endl;
 	std::cout << "*a: " << *a << std::endl;
 
 	std::cout << "=====================" << std::endl;
-	SampleFunc(a);
+	SampleFunc1(a);
 	std::cout << "함수 실행 후 *a: " << *a << std::endl;
 
 	std::cout << "=====================" << std::endl;
-	int* b = SampleFunc();
+	int* b = SampleFunc2();
 	std::cout << "함수에서 생성된 b의 값: " << *b << std::endl;
 
 	*b = 40;
 	std::cout << "함수에서 생성된 b의 값 변경: " << *b << std::endl;
 
+	// 동적 할당된 메모리를 수동으로 해제하는 방법.
+	// delete를 수행하기 전까지는 동적할당된 메모리는 소명되지 않음.
+	// 반드시 해주어야 하는 문법(동적할당 방식을 쓴다면)
 	delete a;
 	delete b;
+
+	int* arr = new int[10];
+	delete[] arr;
+	// delete arr; // 0번만 삭제, 1~9번까지는 유지.
 }
 
-void SampleFunc(int* a)
+void SampleFunc1(int* a)
 {
 	*a = 20;
 	std::cout << "a 변경 -> " << *a << std::endl;
 }
 
-int* SampleFunc()
+int* SampleFunc2()
 {
 	int* b = new int;
 	*b = 30;
--- 3-2/3-2.cpp
+++ 3-2/3-2.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 
-void SampleFunc(int src);
-void SampleFunc(int* src);
+void SampleFunc1(int a);
+void SampleFunc2(int* a);
+int* SampleFunc3();
 
 int main()
 {
 	int a = 10;
-	int* b = &a;
+	int* b = &a; // a변수의 주소 -> b변수(이 떄 변수는 포인터이어야 한다)
 
 	std::cout << "b: " << b << std::endl;
 	std::cout << "*b: " << *b << std::endl;
 
 	std::cout << "=====================" << std::endl;
-	SampleFunc(a);
+	SampleFunc1(a);
 	std::cout << "함수 실행 후 a: " << a << std::endl;
 
 	std::cout << "=====================" << std::endl;
-	SampleFunc(&a);
+	SampleFunc2(b);
 	std::cout << "함수 실행 후 a: " << a << std::endl;
+
+	int* d = SampleFunc3();
+	std::cout << "리턴 d: " << *d << std::endl;
 }
 
-void SampleFunc(int a)
+void SampleFunc1(int a)
 {
 	a = 20;
 	std::cout << "a 변경 -> " << a << std::endl;
 }
 
-void SampleFunc(int* a)
+void SampleFunc2(int* a)
 {
 	*a = 20;
 	std::cout << "a 변경 -> " << *a << std::endl;
+}
+
+int* SampleFunc3()
+{
+	int c = 100; // 지역변수 -> 함수가 종료되면 사라짐.
+	int* d = &c;
+
+	return d;
 }
